d9a23b697922099e2e2ed7b188d3265d54430fa5
Mark Needham <mark.needham@neotechnology.com>
2018-01-01 19:48:14 +0000

    generate new route

--- src/GenerateRoute.js
+++ src/GenerateRoute.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 
 class GenerateRoute extends Component {
@@ -6,18 +7,16 @@ class GenerateRoute extends Component {
     super(props);
     this.state = {
       segments: [],
-      selectedSegment: null,
       startLatitude: props.startLatitude,
       startLongitude: props.startLongitude,
-      estimatedDistance: props.estimatedDistance || 5000
+      estimatedDistance: props.estimatedDistance || 5000,
+      generatedRouteId: null
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(event) {
-    console.log("posted form")
-    console.log(this.state)
     event.preventDefault();
 
     fetch('http://localhost:5000/routes2', {
@@ -25,61 +24,69 @@ class GenerateRoute extends Component {
       headers: { Accept: 'application/json', 'Content-Type': 'application/json'},
       body: JSON.stringify(this.state),
     }).then(results => results.json()).then(data => {
-      console.log(data)
+      this.setState({
+        generatedRouteId: data.routeId
+      })
     }).catch(console.log);
   }
 
   render() {
-    const noSelectedSegment = !this.state.selectedSegment;
-    return (
-      <div>
-      <form action="" method="POST" onSubmit={this.handleSubmit}>
-        <fieldset>
-          <legend>Route details</legend>
+    if(this.state.generatedRouteId) {
+      return ( <Redirect to={{ pathname: `/routes/${this.state.generatedRouteId}`, push: true }}/> )
+    } else {
+      return (
+        <div>
+        <form action="" method="POST" onSubmit={this.handleSubmit}>
+          <fieldset>
+            <legend>Route details</legend>
+            <p>
+              <label htmlFor="segment">Segment</label>
+              <select id="segment" name="segment">
+                <option value="">None</option>
+                {this.state.segments.map(segment =>
+                  <option>foo</option>
+                )}
+
+              </select>
+            </p>
+
+            <p>
+              <label htmlFor="latitude">Latitude</label>
+              <input type="text"
+                     name="latitude"
+                     id="latitude"
+                     defaultValue={this.state.startLatitude}
+                     onChange={ e => this.setState({ startLatitude : e.target.value }) }
+                     size="20" />
+            </p>
+
+            <p>
+              <label htmlFor="longitude">Longitude</label>
+              <input type="text"
+                     name="longitude"
+                     id="longitude"
+                     defaultValue={this.state.startLongitude}
+                     onChange={ e => this.setState({ startLongitude : e.target.value }) }
+                     size="20" />
+            </p>
+
+            <p>
+              <label htmlFor="estimatedDistance">Estimated distance</label>
+              <input type="text"
+                     name="estimatedDistance"
+                     id="estimatedDistance"
+                     defaultValue={this.state.estimatedDistance}
+                     onChange={ e => this.setState({ estimatedDistance : e.target.value }) } />
+            </p>
+
+          </fieldset>
           <p>
-            <label htmlFor="segment">Segment</label>
-            <select id="segment" name="segment">
-              <option value="">None</option>
-              {this.state.segments.map(segment =>
-                <option>foo</option>
-              )}
-
-            </select>
+            <input type="submit" value="Generate route" />
           </p>
-
-          <p>
-            <label htmlFor="latitude">Latitude</label>
-            <input type="text" name="latitude" id="latitude"
-                   defaultValue={this.state.startLatitude}
-                   onChange={ e => this.setState({ startLatitude : e.target.value }) }
-                   size="20" />
-          </p>
-
-          <p>
-            <label htmlFor="longitude">Longitude</label>
-            <input type="text" name="longitude" id="longitude"
-                   defaultValue={this.state.startLongitude}
-                   onChange={ e => this.setState({ startLongitude : e.target.value }) }
-                   size="20" />
-          </p>
-
-          <p>
-            <label htmlFor="estimatedDistance">Estimated distance</label>
-            <input type="text" name="estimatedDistance" id="estimatedDistance"
-                   defaultValue={this.state.estimatedDistance}
-                   onChange={ e => this.setState({ estimatedDistance : e.target.value }) } />
-          </p>
-
-        </fieldset>
-        <p>
-          <input type="hidden" name="shapeLatitude" id="shapeLatitude" />
-          <input type="hidden" name="shapeLongitude" id="shapeLongitude" />
-          <input type="hidden" name="shapeRadius" id="shapeRadius" />
-          <input type="submit" value="Generate route" />
-        </p>
-      </form>
-      </div>
-    )
+        </form>
+        </div>
+      )
+    }
   }
 }
 
