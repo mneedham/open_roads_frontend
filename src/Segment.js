f4c65189d305888db99f0061c651c2b592e0284a
Mark Needham <mark.needham@neotechnology.com>
2018-01-09 20:58:05 +0000

    load segment

--- src/Segment.js
+++ src/Segment.js
@@ -1,12 +1,72 @@
 
 import React, {Component} from "react"
 
+import L from 'leaflet';
+
+import {Map, Polyline, TileLayer} from "react-leaflet"
+
 class Segment extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            roads: []
+        }
+    }
+
+    fetchData( id )
+    {
+        fetch( `http://localhost:5000/segments2/${id}` ).then( results => results.json() ).then( data => {
+            this.setState( {
+                id: id,
+                roads: data.roads.map( rawPoint => [rawPoint["latitude"], rawPoint["longitude"]] ),
+                name: data.name
+            } );
+        } )
+    }
+
+    componentWillReceiveProps( newProps )
+    {
+        this.fetchData( newProps.match.params.id );
+    }
+
+    componentDidMount()
+    {
+        this.fetchData( this.props.match.params.id );
+    }
+
     render() {
         return (
             <div>
+                <div id="main-wrap">
+                    <div id="sidebar">
+                        <p>{this.state.name}</p>
 
-                Segment
+                    </div>
+                    <div id="content-wrap">
+                        {this.state.roads.length > 0 &&
+                        <Map
+                            length={4}
+                            bounds={L.polyline( this.state.roads).getBounds()}
+                            zoom={14}>
+
+                            <TileLayer
+                                attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+
+                            <Polyline
+                                positions={this.state.roads}
+                                color={"blue"}
+                                weight={3}
+                                opacity={.7}
+                                lineJoin={"round"}
+                            />
+
+                        </Map>}
+
+                    </div>
+                </div>
             </div>
         )
     }
